6a5dfe58dcadb03c988821fdb950db4d3df9a742
Jordan Read <jordan.j.read@gmail.com>
2024-10-21 22:59:14 -0400

    Made vertex normals take into account rotation in lighting calculations

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -12,14 +12,21 @@ out vec2 fragTexCoords;
 out float lightIntensity;
 
 void main() {
-    vec3 lightDir = vec3(0, -1, 1);
+    vec4 lightDir = vec4(0, -1, 1, 0);
     lightDir = normalize(lightDir);
     float ambientLight = 0.3;
 
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vertexPos, 1);
     fragTexCoords = texCoords;
-
-    lightIntensity = dot(normal, -lightDir);
+    
+    // Lighting
+    mat4 normalMatrix;
+    normalMatrix[0] = modelMatrix[0];
+    normalMatrix[1] = modelMatrix[1];
+    normalMatrix[2] = modelMatrix[2];
+    normalMatrix[3] = vec4(0, 0, 0, 1);
+    vec4 worldNormal = normalize(normalMatrix * vec4(normal, 0));
+    lightIntensity = dot(worldNormal, -lightDir);
     lightIntensity = clamp(lightIntensity, 0, 1);
     lightIntensity += ambientLight;
     lightIntensity = clamp(lightIntensity, 0, 1);
